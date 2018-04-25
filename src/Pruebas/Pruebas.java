6cc5799e809246af5ecb8256474cd6d9ad6067ff
Mariano <marianomm@rocketmail.com>
2018-04-25 15:51:25 -0600

    Purebas
    Se agrega la clase Purebas para realizar evaluaciones

---
+++ src/Pruebas/Pruebas.java
@@ -0,0 +1,22 @@
+package Pruebas;
+
+import BinarySearchTree.BinarySearchTree;
+
+public class Pruebas {
+    public static void main(String[] args) {
+
+        BinarySearchTree<Integer> bt = new BinarySearchTree<Integer>();
+        bt.insert(1);
+        bt.insert(2);
+        bt.insert(3);
+        bt.insert(4);
+        bt.insert(5);
+        bt.print();
+        System.out.println("x");
+        bt.remove(2);
+        bt.print();
+        System.out.println(bt.findMin());
+        System.out.println(bt.findMax());
+
+    }
+}
